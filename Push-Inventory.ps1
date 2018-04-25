8e97029142f17ee7f30cb528ad4a1f9138b6d8b3
joeypiccola <joey.piccola@gmail.com>
2018-04-25 14:57:29 -0600

    added creds

--- Push-Inventory.ps1
+++ Push-Inventory.ps1
@@ -1,7 +1,15 @@
+$ErrorActionPreference = 'stop'
+
+$vcenter_user = (Get-ChildItem -Path ('env:cred_vcenter_adpiccolaus_USR')).value
+$vcenter_pass = (Get-ChildItem -Path ('env:cred_vcenter_adpiccolaus_PSW')).value
+
+$secpasswd = ConvertTo-SecureString $vcenter_pass -AsPlainText -Force
+$mycreds = New-Object System.Management.Automation.PSCredential ($vcenter_user, $secpasswd)
+
 $path = '.\'
 $files = Get-ChildItem -Path $path -Filter "vcen*"
 $current = $files | Sort-Object -Property lastWriteTime | Select-Object -Last 1
-Copy-Item -Path $current.fullname -Destination '\\nikkor\c$\scripts\vcenter_inventory\vcen_inventory.json' -Force
+Copy-Item -Path $current.fullname -Destination '\\nikkor\c$\scripts\vcenter_inventory\vcen_inventory.json' -Force -Credential $mycreds
 
 if ($files.count -gt 5) {
     $lastThree = $files | Sort-Object -Property lastWriteTime | select -Last 3
