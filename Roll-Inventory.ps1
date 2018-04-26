48f24cc7a389d3e82eafb6d0f095de7dded87401
joeypiccola <joey.piccola@gmail.com>
2018-04-25 22:25:15 -0600

    syntx fix

--- Roll-Inventory.ps1
+++ Roll-Inventory.ps1
@@ -4,7 +4,7 @@ $path = '.\'
 $files = Get-ChildItem -Path $path -Filter "vcen*"
 
 if ($files.count -gt 3) {
-    $lastThree = $files | Sort-Object -Property lastWriteTime | select -Last 3
+    $lastThree = $files | Sort-Object -Property lastWriteTime | Select-Object -Last 3
     $olderFiles = Compare-Object -ReferenceObject $files -DifferenceObject $lastThree -PassThru
     foreach ($file in $olderFiles) {
         Write-Host "removing $($file.fulname)"
